1bb739bb789b0a8de09a114c8522b7e99d25cb4f
Michael Bayne <mdb@samskivert.com>
2014-04-07 10:20:22 -0700

    Require Java 1.7 and use Throwable.addSuppressed.

--- src/main/scala/reactual/ReactionException.scala
+++ src/main/scala/reactual/ReactionException.scala
@@ -10,6 +10,8 @@ import java.io.{PrintStream, PrintWriter}
 /** Communicates failures from one or more reactors. */
 class ReactionException (val failures :Seq[Throwable]) extends RuntimeException {
 
+  failures foreach addSuppressed
+
   override def getMessage = {
     val buf = new StringBuilder
     for (failure <- failures) {
@@ -17,14 +19,6 @@ class ReactionException (val failures :Seq[Throwable]) extends RuntimeException 
       buf.append(failure.getClass.getName).append(": ").append(failure.getMessage)
     }
     failures.size + " failures: " + buf
-  }
-
-  override def printStackTrace (s :PrintStream) {
-    failures foreach { _.printStackTrace(s) }
-  }
-
-  override def printStackTrace (w :PrintWriter) {
-    failures foreach { _.printStackTrace(w) }
   }
 
   override def fillInStackTrace () :Throwable = this // no stack trace here
